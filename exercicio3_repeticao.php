c47d687f737d0c33831d12fcd412b065eb821665
camillycristina <camillycristinasilva10@gmail.com>
2022-05-25 12:23:59 -0300

    salva

---
+++ exercicio3_repeticao.php
@@ -0,0 +1,20 @@
+<?php
+ //faça um programa em PHP que faça uma contagem de um valor a outra informados pelo usuario via form, o incremento tambem deve ser informado no form;
+ 
+ $numero = 0;
+ $numero2 = 10;
+
+ while($numero <=10){
+     echo $numero . "</br>";
+     $numero++; 
+ }
+
+ echo"</br>";
+ while($numero2 >=0):
+    echo $numero2 . "</br>";
+    $numero2--;
+ endwhile;
+
+?>
+ 
+?>

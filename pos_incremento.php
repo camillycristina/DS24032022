b9b827d741a59d71140c146fb88a2cb47f485548
camillycristina <camillyCristinasilva10@gmail.com>
2022-05-25 07:48:48 -0300

    salva

---
+++ pos_incremento.php
@@ -0,0 +1,10 @@
+<?php
+
+ $a = 0;
+
+ //Pós-incremento
+ echo"O valor de a é $a</br>;"
+ $a++;
+ echo"O novo valor de a é $a";
+
+?>
---
+++ pre_decremento.php
@@ -0,0 +1,8 @@
+<?php
+
+ $a = 10;
+
+ //Pré-decremento
+ echo"O valor de a é $a e o novo valor é" . --$a;
+
+?>
